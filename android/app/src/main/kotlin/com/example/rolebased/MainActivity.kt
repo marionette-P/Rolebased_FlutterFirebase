c25989a046c419dda436a9ea1625b9eb2f7f23c0
paez-marie1019 <paez.marie1019@gmail.com>
2024-04-11 22:08:39 +0800

    flutter

---
+++ android/app/src/main/kotlin/com/example/rolebased/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rolebased
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:rolebased/firebase_options.dart';
+
+import 'register.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue[900],
+      ),
+      home: Register(),
+    );
+  }
+}
